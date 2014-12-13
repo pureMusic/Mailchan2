c7375108b272feed3f4e6f0a725c4b9c5d450d73
HAZENFV <nanorhex@yahoo.co.jp>
2014-12-13 23:38:43 +0900

    敵追加
    画像追加３種
    メッソールのコントロール設定

--- MailChan/Assets/Scripts/EnemyCtrl.cs
+++ MailChan/Assets/Scripts/EnemyCtrl.cs
@@ -12,6 +12,10 @@ public class EnemyCtrl : MonoBehaviour {
 		public GameObject bullet;		//Bulletのプレハブ
 		public bool faceRight = false;	//向いてる方向
 		public GameObject destroy;		//消滅エフェクトのプレハブ
+		public bool moveFlag;			//移動フラグ
+		public float moveSpeed;			//移動速度
+		public bool animeFlag;			//個別アニメーション用フラグ
+		public bool guardFlag = false;	//ガード状態フラグ
 
 
 		// Use this for initialization
@@ -22,7 +26,13 @@ public class EnemyCtrl : MonoBehaviour {
 		
 		// Update is called once per frame
 		void Update () {
-		
+				if (moveFlag) {
+						transform.position = new Vector3 (transform.position.x + moveSpeed * (faceRight ? 1 : -1),
+															transform.position.y,
+															transform.position.z);
+				}
+				//アニメーション用フラグを設定
+				GetComponent<Animator> ().SetBool ("animeFlag", animeFlag);
 		}
 
 		//行動制御---------------------------------------------------------------------
@@ -47,22 +57,62 @@ public class EnemyCtrl : MonoBehaviour {
 				}
 		}
 
+		//メッソール
+		IEnumerator Pattern02(){
+				int counter = 4;	//ガード時間
+				guardFlag = true;
+
+				while (true) {
+						yield return new WaitForSeconds (counter);
+
+						//攻撃開始
+						animeFlag = true;
+						yield return new WaitForSeconds (1);
+						setFaceRight ();
+						moveFlag = true;
+						guardFlag = false;
+						Vector3 v3 = this.transform.position;
+						GameObject bulletCtrl = Instantiate (bullet, v3, this.transform.localRotation) as GameObject;
+						Bullet b = bulletCtrl.GetComponent<Bullet> ();
+						b.BulletCtrl (1, faceRight);
+						yield return new WaitForSeconds (1);
+						moveFlag = false;
+						guardFlag = true;
+						yield return new WaitForSeconds (1);
+						animeFlag = false;
+				}
+		}
+
 		//ダメージ制御-------------------------------------------------------------
 		public void Damage(int damage){
-				lifePoint -= damage;
+				if (!guardFlag) {
+						lifePoint -= damage;
 
-				//死亡判定
-				if (lifePoint <= 0) {
-						//消滅エフェクト
-						destroy = Instantiate (destroy, this.transform.position, this.transform.rotation) as GameObject;
-						Destroy (destroy, 1 / 6f);	//10F
+						//死亡判定
+						if (lifePoint <= 0) {
+								//消滅エフェクト
+								destroy = Instantiate (destroy, this.transform.position, this.transform.rotation) as GameObject;
+								Destroy (destroy, 1 / 6f);	//10F
 
-						//オブジェクト削除
-						Destroy (this.gameObject);
+								//オブジェクト削除
+								Destroy (this.gameObject);
+						}
 				}
 		}
 
 		//共通処理----------------------------------------------------------------------
+
+		//向きチェック
+		void setFaceRight(){
+				GameObject target = GameObject.Find ("Player");
+				if (target.transform.position.x > this.transform.position.x) {
+						faceRight = true;
+						this.transform.rotation = new Quaternion(0,180,0,0); 
+				} else {
+						faceRight = false;
+						this.transform.rotation = new Quaternion(0,0,0,0); 
+				}
+		}
 
 		//接触時の処理
 		void OnTriggerEnter2D(Collider2D col){
